109d73489fd606eab2e683563a5b3a85759fe385
Cashew <tungbinem@gmail.com>
2022-09-01 23:21:58 +0900

    Added English word search

--- cashewbot/commandPages/createHelpPage.js
+++ cashewbot/commandPages/createHelpPage.js
@@ -8,7 +8,9 @@ function helpPageEmbed() {
         .setDescription('My commands:')
         .addFields(
 
-            { name: '[s', value: 'Search for a Chinese or English word on CC-CEDICT' },
+            { name: '[search (alias: [s)', value: 'Search for a Chinese or English word on CC-CEDICT' },
+            { name: '[kyuji (alias: [k)', value: 'Convert a Japanese sentence from [Shinjitai](https://en.wikipedia.org/wiki/Shinjitai)(新字体) to [Kyujitai](https://en.wikipedia.org/wiki/Ky%C5%ABjitai)(旧字体) and vice versa'},
+            { name: '[help (alias: [h)', value: 'How did I get here?' },
 
         )
         .setFooter({ text: 'You can click on the reactions below to see more commands', iconURL: 'https://i.postimg.cc/W3FjFhDt/Red-Bird.jpgc' });
--- cashewbot/commandPages/createMandarinSearchPage.js
+++ cashewbot/commandPages/createMandarinSearchPage.js
@@ -1,4 +1,4 @@
-const tokenize = require('chinese-tokenizer').loadFile('./assets/cedict_ts.u8')
+const chineseLexicon = require('chinese-lexicon')
 const { EmbedBuilder } = require('discord.js');
 
 const haveSameData = function(obj1, obj2) {
@@ -23,30 +23,38 @@ const removeDuplicate = function(array) {
             }
         }
     }
-    return array
+    return array;
 }
-        
+function isAlpha(str) {
+    return /^[a-zA-Z]+$/.test(str);
+  }
 const wordSearchEmbed = new EmbedBuilder()
 	.setColor(0x0099FF) //Sky Blue
 	.setFooter({ text: 'You can click the reactions below to see more information!', iconURL: 'https://i.postimg.cc/W3FjFhDt/Red-Bird.jpg' });
 
 const returnLookUpWordEmbed = async function(message) {
 
-    const embed = new EmbedBuilder(wordSearchEmbed.data)
-    
-    var wordInfo = tokenize(message)[0]
+    const embed = new EmbedBuilder(wordSearchEmbed.data);
+    embed.setTitle(`Search results for "${message}"`);
 
-    var simplified = wordInfo.simplified
-    var traditional = wordInfo.traditional
-    var matches = wordInfo.matches
-    matches = removeDuplicate(matches)
-         
-    embed.setTitle(`Search results for "${message}"`)
-    //.setURL
-    for (var i in matches) {
-        count = parseInt(i) + 1
+    // If the searched word is alphabetical, search the matching chinese entries
+    if (isAlpha(message)) {
+        var wordInfo = await chineseLexicon.search(message)
+    }
+    else {
+        var wordInfo = await chineseLexicon.getEntries(message)
+    }
+
+    for (let i = 0; i < Math.min(wordInfo.length, 4); i++) {
+
+        var simplified = wordInfo[i].simp
+        var traditional = wordInfo[i].trad
+        var definitions = wordInfo[i].definitions
+        var pinyin = wordInfo[i].pinyin
+        var statistics = wordInfo[i].statistics
+
         embed.addFields(
-            {name: `${simplified} | ${traditional}`, value: `${count}. (${matches[i].pinyinPretty}) ${matches[i].english}`},
+            {name: `${simplified} | ${traditional}`, value: `\`Hsk Level: ${statistics.HskLevel}\`\n(${pinyin}) *${definitions.join(', ')}*`},
         )
 
     }

6f45cb3829ac59c0f9336eb8c76bddcd714228fa
Cashew <tungbinem@gmail.com>
2022-09-02 14:55:44 +0900

    heroku

--- cashewbot/main.js
+++ cashewbot/main.js
@@ -83,3 +83,10 @@ client.once('ready', c => {
 
 // Login to Discord with your client's token
 client.login(process.env.botToken);
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
